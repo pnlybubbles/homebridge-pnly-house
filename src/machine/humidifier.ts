93b64fce4d43b58fa826951ee606de1536c984bd
pnlybubbles <pnlybubbles@gmail.com>
2021-02-21 04:17:39 +0900

    グチャグチャ操作したときにちゃんと整合を取るように

--- src/machine/humidifier.ts
+++ src/machine/humidifier.ts
@@ -1,6 +1,6 @@
 import { PlatformConfig, Logger } from 'homebridge';
 import { commandDevice } from '../api';
-import { unreachable } from '../util';
+import { delay, unreachable } from '../util';
 
 /**
  * min: 30
@@ -100,23 +100,50 @@ export class Humidifier {
 
   // 湿度設定
 
+  /**
+   * 湿度設定に入っている場合true
+   */
+  private _hotHumidityTransition = false;
+  /**
+   * 湿度設定用の重要なリクエスト中の場合true (リクエストの間隔が短くなりすぎないように調節するため)
+   */
+  private _requestingHumidityTransition = false;
+  /**
+   * 湿度設定用の重要なリクエスト中、きりの良いタイミングでリクエストを中止するための関数
+   * 中止した場合はresolveが呼ばれる
+   */
+  private _abortHumidityTransition?: { resolve: () => void; reject: () => void };
+
   private async humidityTransition(target: Humidity) {
     if (this.state.targetHumidityInternal === target) {
       return;
     }
 
-    // 湿度設定モードに入るための初回リクエスト
-    try {
-      const command = this.config.mapping.humidity;
-      await commandDevice({ token: this.config.token, deviceId: this.deviveId, command, commandType: 'customize', parameter: 'default' });
-    } catch (e) {
-      this.log.debug(e);
-      return;
+    this._requestingHumidityTransition = true;
+
+    if (!this._hotHumidityTransition) {
+      // 湿度設定モードに入るための初回リクエスト
+      try {
+        const command = this.config.mapping.humidity;
+        await commandDevice({ token: this.config.token, deviceId: this.deviveId, command, commandType: 'customize', parameter: 'default' });
+      } catch (e) {
+        this.log.debug(e);
+        return;
+      }
+
+      this._hotHumidityTransition = true;
     }
 
     // eslint-disable-next-line no-constant-condition
     while (true) {
       const current = this.state.targetHumidityInternal;
+
+      if (this._abortHumidityTransition) {
+        this._requestingHumidityTransition = false;
+        this._abortHumidityTransition.resolve();
+        this._abortHumidityTransition = undefined;
+        return;
+      }
 
       if (current === target) {
         break;
@@ -138,6 +165,11 @@ export class Humidifier {
         this.state.targetHumidityInternal = current;
       }
     }
+
+    this._requestingHumidityTransition = false;
+    // 湿度設定モードから出るための待機
+    await delay(3);
+    this._hotHumidityTransition = false;
   }
 
   getTargetHumidity(): number {
@@ -149,6 +181,17 @@ export class Humidifier {
 
     if (!this.state.active) {
       return;
+    }
+
+    if (this._requestingHumidityTransition) {
+      if (this._abortHumidityTransition) {
+        this._abortHumidityTransition.reject();
+      }
+      try {
+        await new Promise<void>((resolve, reject) => this._abortHumidityTransition = { resolve, reject });
+      } catch {
+        return;
+      }
     }
 
     const target = Math.max(Math.min(Math.floor(value / 5) * 5, 90), 30) as Humidity;
--- src/util.ts
+++ src/util.ts
@@ -1,3 +1,7 @@
 export function unreachable(_: never): never {
   throw new Error(`unreachable (${_})`);
 }
+
+export function delay(second: number) {
+  return new Promise(resolve => setTimeout(resolve, second * 1000));
+}
